5a3c355ff536528883c22a16f91c17bc71b53691
nicochidt <nico.chiaraviglio@gmail.com>
2017-12-13 17:39:17 -0300

    A few screens added

--- HiEnds.py
+++ HiEnds.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import wx
+import wx.grid as gridlib
 
 class HiEnds(wx.Panel):
     
@@ -11,7 +12,6 @@ class HiEnds(wx.Panel):
     def _esofago(self):
         box = wx.StaticBox(self, wx.ID_ANY, "Esófago")
         esofSizer = wx.StaticBoxSizer( box, wx.VERTICAL)
-        
         
         #globalSizer = wx.BoxSizer(wx.VERTICAL)
         CMSizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -26,9 +26,6 @@ class HiEnds(wx.Panel):
         
         esofSizer.Add(CMSizer)
         
-        return esofSizer
-    
-    def _mucosa(self):
         box = wx.StaticBox(self, wx.ID_ANY, "Mucosa")
         mucoSizer = wx.StaticBoxSizer(box)     
         
@@ -115,25 +112,182 @@ class HiEnds(wx.Panel):
         
         box = wx.StaticBox(self, wx.ID_ANY, "Otros")
         Sizer = wx.StaticBoxSizer(box)
-        self.other = wx.TextCtrl(self, size = (300,60), style= wx.TE_MULTILINE )
+        self.other = wx.TextCtrl(self, size = (200,60), style= wx.TE_MULTILINE )
         self.abnormalWid.append(self.other)
         Sizer.Add(self.other)
         stSizer.Add(Sizer, pos = (6,0), span= (1,4))
         
         mucoSizer.Add(stSizer)
+        esofSizer.Add(mucoSizer)
         
-        return mucoSizer
+        return esofSizer
+    
+    
+    def _estomago(self):
+        
+        ## Lesiones sizer
+        box = wx.StaticBox(self, wx.ID_ANY, "Estómago")
+        boxSizer = wx.StaticBoxSizer(box)
+        vBoxSizer = wx.BoxSizer(wx.VERTICAL)
+        
+        box = wx.StaticBox(self, wx.ID_ANY, "Mucosa")
+        mucosaSizer = wx.StaticBoxSizer(box)
+        
+        sizer = wx.GridBagSizer(hgap = 5, vgap = 5)
+        tags = ['Normal', 'Edema', 'Eritema', 'Micronodular', 'Otro']
+        for i,j in enumerate(tags):
+            tx = wx.StaticText(self, label = j)
+            sizer.Add(tx, pos = (0, i + 1), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+        self.est_mucosa = {}
+        loc = ['Antro', 'Cuerpo', 'Fundus']
+        
+        for i,j in enumerate(loc): 
+            tx = wx.StaticText(self, label = j)
+            sizer.Add(tx, pos = (i+1, 0))
+            for k in range(0, len(tags) -1):
+                cb = wx.CheckBox(self)
+                sizer.Add(cb, pos = (i + 1, k + 1), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+                self.est_mucosa.setdefault(j,[]).append(cb)
+            cb = wx.TextCtrl(self, size = (90, 20))
+            sizer.Add(cb, pos = (i + 1, len(tags)))
+            self.est_mucosa[j].append(cb)
+            
+        mucosaSizer.Add(sizer)    
+        vBoxSizer.Add(mucosaSizer)
+        
+        
+        ## Lesiones section
+        box = wx.StaticBox(self, wx.ID_ANY, "Lesiones")
+        lesSizer = wx.StaticBoxSizer(box)
+        lessionSizer = wx.BoxSizer(wx.VERTICAL)
+        
+        
+        # Ulceras section
+        box = wx.StaticBox(self, wx.ID_ANY, "Ulceras")
+        ulSizer = wx.StaticBoxSizer(box)
+        sizer = wx.GridBagSizer(hgap = 5, vgap = 5)
+        
+        tx = wx.StaticText(self, label = "Cantidad")
+        sizer.Add(tx, pos = (1,0), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+        tx = wx.StaticText(self, label = "Tamaños")
+        sizer.Add(tx, pos = (2,0), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+        
+        tags = ['Antro', 'Curvatura Mayor', 'Curvatura Menor', 'Fundus']
+        self.est_lesiones = {}
+        self.est_lesiones['ulceras'] = {}
+        self.est_lesiones['ulceras']['pos'] = []
+        self.est_lesiones['ulceras']['size'] = []
+        
+        for i,j in enumerate(tags):
+            tx = wx.StaticText(self, label = j)
+            bx = wx.TextCtrl(self, size = (25,20))
+            sizer.Add(tx, pos = (0, i + 1))
+            sizer.Add(bx, pos = (1, i + 1), flag=wx.ALIGN_CENTER_HORIZONTAL)
+            self.est_lesiones['ulceras']['pos'].append(bx)
+            bx = wx.TextCtrl(self, size = (25,20))
+            sizer.Add(bx, pos = (2, i + 1), flag=wx.ALIGN_CENTER_HORIZONTAL)
+            self.est_lesiones['ulceras']['size'].append(bx)
+        
+        
+        forrest = ['Ia', 'Ib', 'IIa', 'IIb', 'IIc', 'III', 'N/A']
+        self.est_lesiones['ulceras']['forrest'] = []
+        fSizer = wx.BoxSizer(wx.HORIZONTAL)
+        tx = wx.StaticText(self, label = "Forrest")
+        fSizer.Add(tx, 4, wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 4)
+        fSizer.Add((10,10))
+        for i,j in enumerate(forrest):
+            tx = wx.RadioButton(self, label = j)
+            self.est_lesiones['ulceras']['forrest'].append(tx)
+            fSizer.Add(tx, 4, wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 4)
+        
+        tx = wx.StaticText(self, label = 'Otros')
+        sizer.Add(tx, pos = (4,0))
+        self.est_lesiones['ulceras']['otros'] = wx.TextCtrl(self, size = (300, 20))
+        sizer.Add(self.est_lesiones['ulceras']['otros'], pos = (4,1), span = (1,4))
+        sizer.Add(fSizer, pos = (3,0), span = (1,6))
+        ulSizer.Add(sizer)    
+        lessionSizer.Add(ulSizer)
+        
+        
+        # Varices section
+        box = wx.StaticBox(self, wx.ID_ANY, "Varices")
+        varSizer = wx.StaticBoxSizer(box)
+        varicesSizer = wx.BoxSizer(wx.HORIZONTAL)
+        types = ['IGV I', 'IGV II', 'GOV I', 'GOV II']
+        self.est_lesiones['varices'] = {}
+        self.est_lesiones['varices']['names'] = types
+        self.est_lesiones['varices']['checks'] = []
+        
+        for i in types:
+            bt = wx.CheckBox(self, label = i)
+            varicesSizer.Add(bt, 0, wx.ALL, 5)
+            self.est_lesiones['varices']['checks'].append(bt)
+        
+        
+        # Polipo
+        box = wx.StaticBox(self, wx.ID_ANY, "Polipos")
+        polSizer = wx.StaticBoxSizer(box)
+        sizer = wx.GridBagSizer(hgap = 5, vgap = 5)
+        
+        tx = wx.StaticText(self, label = "Cantidad")
+        sizer.Add(tx, pos = (1,0), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+        tx = wx.StaticText(self, label = "Tamaños")
+        sizer.Add(tx, pos = (2,0), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+        
+        tags = ['Antro', 'Curvatura Mayor', 'Curvatura Menor', 'Fundus']
+        self.est_lesiones = {}
+        self.est_lesiones['polipos'] = {}
+        self.est_lesiones['polipos']['pos'] = []
+        self.est_lesiones['polipos']['size'] = []
+        
+        for i,j in enumerate(tags):
+            tx = wx.StaticText(self, label = j)
+            bx = wx.TextCtrl(self, size = (25,20))
+            sizer.Add(tx, pos = (0, i + 1))
+            sizer.Add(bx, pos = (1, i + 1), flag=wx.ALIGN_CENTER_HORIZONTAL)
+            self.est_lesiones['polipos']['pos'].append(bx)
+            bx = wx.TextCtrl(self, size = (25,20))
+            sizer.Add(bx, pos = (2, i + 1), flag=wx.ALIGN_CENTER_HORIZONTAL)
+            self.est_lesiones['polipos']['size'].append(bx)
+        
+        tx = wx.StaticText(self, label = 'Otros')
+        sizer.Add(tx, pos = (4,0))
+        self.est_lesiones['polipos']['otros'] = wx.TextCtrl(self, size = (300, 20))
+        sizer.Add(self.est_lesiones['polipos']['otros'], pos = (4,1), span = (1,4))
+        polSizer.Add(sizer)    
+        
+        
+        # Other section
+        box = wx.StaticBox(self, wx.ID_ANY, "Otro")
+        otherSizer = wx.StaticBoxSizer(box)
+        self.est_lesiones['other'] = wx.TextCtrl(self, size = (400, 40), style = wx.TE_MULTILINE)
+        otherSizer.Add(self.est_lesiones['other'])
+        
+        
+        
+        varSizer.Add(varicesSizer)
+        lessionSizer.Add(varSizer)
+        
+        
+        lessionSizer.Add(polSizer)
+        lessionSizer.Add(otherSizer)
+        lesSizer.Add(lessionSizer)
+        
+        vBoxSizer.Add(lesSizer)
+        boxSizer.Add(vBoxSizer)
+        
+        
+        
+        
+        return boxSizer
+    
+    def _gridPol(self):
+        pass
     
     def OnNormal(self, event):
-        
-        print event
-        print dir(event)
-        print event.Checked()
         if event.Checked() == True:
-            
             for i in self.abnormalWid:
                 i.Disable()
-        
         else:
             for i in self.abnormalWid:
                 i.Enable()
@@ -143,26 +297,135 @@ class HiEnds(wx.Panel):
         
         box = wx.StaticBox(self, wx.ID_ANY, "Motivo de estudio")
         Sizer = wx.StaticBoxSizer(box)
-        self.reason = wx.TextCtrl(self, size = (900,60), style = wx.TE_MULTILINE)
+        self.reason = wx.TextCtrl(self, size = (1250,60), style = wx.TE_MULTILINE)
         Sizer.Add(self.reason)
         
         return Sizer
+    
+     
+    def _duodeno(self):
+        box = wx.StaticBox(self, wx.ID_ANY, "Duodeno")
+        duExtSizer = wx.StaticBoxSizer( box, wx.VERTICAL)
+        duBoxSizer = wx.BoxSizer(wx.VERTICAL)
         
+        box = wx.StaticBox(self, wx.ID_ANY, "Mucosa")
+        mucStSizer = wx.StaticBoxSizer( box, wx.VERTICAL)
+        mucBoxSizer = wx.GridBagSizer(vgap = 5, hgap = 5)
+        
+        
+        self.duo_mucosa = []
+        tags = ['Normal', 'Edema', 'Dism. Pliegues', 'Dism. Vellosidades']
+        for i,j in enumerate(tags):
+            ch = wx.CheckBox(self, label = j)
+            mucBoxSizer.Add(ch, pos = (0, i))
+            self.duo_mucosa.append(ch)
+        self.duo_mucosa[0].Bind(wx.EVT_CHECKBOX, self.OnDuodenoNormal)
+        mucStSizer.Add(mucBoxSizer)
+        
+        
+        box = wx.StaticBox(self, wx.ID_ANY, "Lesiones")
+        lessStSizer = wx.StaticBoxSizer( box, wx.VERTICAL)
+        lessBoxSizer = wx.BoxSizer(wx.VERTICAL)
+        
+        box = wx.StaticBox(self, wx.ID_ANY, "Ulceras")
+        ulcStSizer = wx.StaticBoxSizer( box, wx.VERTICAL)
+        ulcBoxSizer = wx.GridBagSizer(vgap = 5, hgap = 5)
+        
+        tag = ['Bulbo', '2da Porción', '3era Porción']
+        self.duo_lesiones = {}
+        self.duo_lesiones['ulceras'] = {}
+        self.duo_lesiones['ulceras']['pos'] = []
+        self.duo_lesiones['ulceras']['forrest'] = []
+         
+        for i,j in enumerate(tag): 
+            tx = wx.StaticText(self, label = j)
+            ulcBoxSizer.Add(tx, pos = (0,i))
+            bx = wx.TextCtrl(self, size = (25,20))
+            ulcBoxSizer.Add(bx, pos = (1,i), flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL)
+            self.duo_lesiones['ulceras']['pos'].append(bx)
+        
+        tx = wx.StaticText(self, label = 'Tamaños (cm)')
+        ulcBoxSizer.Add(tx, pos = (2,0))
+        bx = wx.TextCtrl(self, size = (200,20))
+        ulcBoxSizer.Add(bx, pos = (2,1), span = (1,3))
+        self.duo_lesiones['ulceras']['size'] = bx
+        
+        
+        forrest = ['Ia', 'Ib', 'IIa', 'IIb', 'IIc', 'III', 'N/A']
+        self.duo_lesiones['ulceras']['forrest'] = []
+        fSizer = wx.BoxSizer(wx.HORIZONTAL)
+        tx = wx.StaticText(self, label = "Forrest")
+        fSizer.Add(tx, 4, wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 4)
+        fSizer.Add((10,10))
+        for i,j in enumerate(forrest):
+            tx = wx.RadioButton(self, label = j)
+            self.duo_lesiones['ulceras']['forrest'].append(tx)
+            fSizer.Add(tx, 4, wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 4)
+        
+        tx = wx.StaticText(self, label = "Otro")
+        self.duo_lesiones['ulceras']['otro'] = wx.TextCtrl(self, size= (200, 20), style = wx.TE_MULTILINE)
+        
+        ulcBoxSizer.Add(fSizer, pos = (3,0), span =(1,4))
+        ulcBoxSizer.Add(tx, pos = (4,0))
+        ulcBoxSizer.Add(self.duo_lesiones['ulceras']['otro'], pos = (4,1), span = (1,4))
+        
+        ulcStSizer.Add(ulcBoxSizer)
+        
+        # Other section
+        box = wx.StaticBox(self, wx.ID_ANY, "Otro")
+        otherSizer = wx.StaticBoxSizer(box)
+        self.duo_lesiones['other'] = wx.TextCtrl(self, size = (400, 40), style = wx.TE_MULTILINE)
+        otherSizer.Add(self.duo_lesiones['other'])
+        
+        
+        
+        lessBoxSizer.Add(ulcStSizer)
+        lessBoxSizer.Add(otherSizer)
+        lessStSizer.Add(lessBoxSizer)
+        
+        
+        duBoxSizer.Add(mucStSizer)
+        duBoxSizer.Add(lessStSizer)
+        duExtSizer.Add(duBoxSizer)
+        return duExtSizer
+        
+        
+    def OnDuodenoNormal(self, event):
+        if event.Checked() == True:
+            for i in self.duo_mucosa:
+                i.Disable()
+            self.duo_mucosa[0].Enable()
+        else:
+            for i in self.duo_mucosa:
+                i.Enable()
+    
     def MainWindow(self):
   
-        globalSizer = wx.BoxSizer(wx.VERTICAL)
+        #globalSizer = wx.BoxSizer(wx.VERTICAL)
+        globalSizer = wx.GridBagSizer(hgap = 5, vgap = 5)
     
         reasonSizer = self._reason()
         esofSizer = self._esofago()
-        globalSizer.Add((10,10), 0)
-        mucoSizer = self._mucosa()
-        esofSizer.Add(mucoSizer)
+        #globalSizer.Add((10,10), 0)
+        estogSizer = self._estomago()
+        duodeno = self._duodeno()
         
-        globalSizer.Add(reasonSizer)
-        globalSizer.Add(esofSizer)
-        #globalSizer.Add(mucoSizer)
+        globalSizer.Add(reasonSizer, pos =(0,0), span = (1,3))
+        globalSizer.Add(esofSizer, pos = (1,0))
+        globalSizer.Add(estogSizer, pos = (1,1))
+        globalSizer.Add(duodeno, pos = (1,2))
+        
+        #globalSizer.Add(reasonSizer)
+        #globalSizer.Add(esofSizer)
         self.SetSizer(globalSizer)
         globalSizer.Fit(self)
         
             
-            
+class PolipoEstomago():
+    def __init__(self, loc, size, cecil= None, pediculado= None, otro = None):
+        self.loc = loc
+        self.size = size
+        self.cecil = cecil
+        self.pediculado = pediculado
+        self.otro = otro
+    
